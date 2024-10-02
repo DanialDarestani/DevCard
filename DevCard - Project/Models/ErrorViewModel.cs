62ae5eda9b87729a5d0468c2a656b836b7883eb4
Danial Darestani <danialdf81@outlook.com>
2024-10-02 20:34:23 +0330

    a

---
+++ DevCard - Project/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevCard___Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
